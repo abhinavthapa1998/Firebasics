d9cf675e0269b52a83d45150b5f824fb51925575
Abhinav Thapa <abhinavthapa1998@gmail.com>
2022-04-13 15:38:35 +0530

    commit

--- pages/Contact/index.jsx
+++ pages/Contact/index.jsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 import { useRouter } from "next/router";
 import NavbarLogIn from "../../components/NavbarLogIn";
 import { userAccessToken } from "../utils/fetchUserDetails";
-const index = () => {
+const index = (props) => {
   const router = useRouter();
   useEffect(() => {
     const accessToken = userAccessToken();
@@ -16,8 +16,19 @@ const index = () => {
     <>
       <NavbarLogIn />
       <h1>Contact us!</h1>;
+      <img src={props.contact} />
     </>
   );
 };
 
 export default index;
+
+export async function getStaticProps() {
+  const contact =
+    "https://images.unsplash.com/photo-1596524430615-b46475ddff6e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80";
+  return {
+    props: {
+      contact,
+    },
+  };
+}
--- pages/Pricing/index.jsx
+++ pages/Pricing/index.jsx
@@ -3,7 +3,8 @@ import { useEffect } from "react";
 import { useRouter } from "next/router";
 import NavbarLogIn from "../../components/NavbarLogIn";
 import { userAccessToken } from "../utils/fetchUserDetails";
-const index = () => {
+
+const index = (props) => {
   const router = useRouter();
   useEffect(() => {
     const accessToken = userAccessToken();
@@ -16,8 +17,18 @@ const index = () => {
     <>
       <NavbarLogIn />
       <h1>Pricing</h1>;
+      <img src={props.prices} />
     </>
   );
 };
 
 export default index;
+export async function getStaticProps() {
+  const prices =
+    "https://images.unsplash.com/photo-1553729459-efe14ef6055d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80";
+  return {
+    props: {
+      prices,
+    },
+  };
+}
