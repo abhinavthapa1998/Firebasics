f2dd42d0f84a62181677dc6164b97525f7db56c1
Abhinav Thapa <abhinavthapa1998@gmail.com>
2022-04-13 16:47:43 +0530

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import "../styles/index.css";
-
+import Head from "next/head";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>FireBasics</title>
+        <link
+          rel="icon"
+          href="https://cdn-icons.flaticon.com/png/512/2403/premium/2403711.png?token=exp=1649849411~hmac=9deee6e4945ce03eb6ef1a0711623d20"
+        />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
